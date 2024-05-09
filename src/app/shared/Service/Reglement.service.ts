907efa90b2e9ff5fb07e21960ea3d9ead622e184
BOULARES Mohamedridha <74650380+ridha-boulares@users.noreply.github.com>
2024-05-09 10:28:44 +0200

    Update Reglement.service.ts

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://192.168.56.2:8089/SpringMVC/reglement';
+  readonly API_URL = 'http://service-back:8089/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
